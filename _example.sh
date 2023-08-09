54849d6e87c2c313464eaa41294cbb0be0764747
Keith Chu <cqroot@outlook.com>
2023-08-10 00:16:37 +0800

    feat: add example

---
+++ _example.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+source "${SCRIPT_DIR}/slog.sh"
+
+function log_all() {
+	slog_error "Error message."
+	slog_warning "Warning message."
+	slog_info "Info message."
+	slog_debug "Debug message."
+	echo
+}
+
+echo "DEFAULT LEVEL"
+log_all
+
+echo "ERROR LEVEL"
+SLOG_LEVEL=${SLOG_LEVEL_ERROR}
+log_all
+
+echo "DEBUG LEVEL"
+SLOG_LEVEL=${SLOG_LEVEL_DEBUG}
+log_all
